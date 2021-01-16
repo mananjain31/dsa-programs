9309eab4d2e781ee68883a54c46006dcc301a420
mananjain31 <mananjain31jan@gmail.com>
2021-01-16 20:22:59 +0530

    completed pallindrome > withGlobalVar.c

--- pallindrome/withGlobalVar.c
+++ pallindrome/withGlobalVar.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
-int r = 2;
+int r=0;
+void rev(int num)
+{
+	if(num==0)return;
+	r = r*10 + num%10;
+	num/=10;
+	rev(num);	
+}
 void main()
 {
-	int num=1;
+	int num;
 	printf("Enter number to check pallindrome: ");
 	scanf("%d", &num);
-	// rev(num);
+	rev(num);
 	if(num==r)printf("the number is pallindrome.\n");
 	else printf("the number is not pallindrome\n");
 }
