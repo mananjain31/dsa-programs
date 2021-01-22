4724bdcc60537cbdad736618be5a67e1099a5b87
mananjain31 <mananjain31jan@gmail.com>
2021-01-22 12:51:29 +0530

    modified pallindrome > withoutLoop.c

--- pallindrome/withoutLoop.c
+++ pallindrome/withoutLoop.c
@@ -15,4 +15,29 @@ void main()
 	else printf("\nGiven no. is not pallindrome.\n");
 }
 
+/*
+let no n -> 1234
+show(1234, 0)
+{
+ s = 0*10 + 1234%10 = 4
+ return show(123, 4)
+ {
+   s = 4*10 + 123%10 = 43
+   return show (12, 43)
+   {
+	s = 43*10 + 12%10 = 432
+	return show(1, 432)
+    {
+	 s = 432*10 + 1%10 = 4321
+	 return show(0, 4321)
+	 {
+	   if(n==0) return 4321
+	 }
+	}
+   }
+ } 
+}
+
+*/
+
 
