a9b7d346c9b280239bbabf57401e4448ab1a8895
mananjain31 <mananjain31jan@gmail.com>
2021-01-22 21:37:34 +0530

    added mergeSort function in mergeSort.c

--- merge sort/mergeSort.c
+++ merge sort/mergeSort.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
-void split();
+void mergeSort(int[],int);
 void merge();
 void display(int[],int);
+
+void mergeSort(int x[], int n)
+{
+	if(n==1)return;
+	int i;
+	int m1 = n/2;
+	int m2 = n-m1;
+	int a[m1],b[m2];
+	for(i=0;i<m1;i++) a[i] = x[i];
+	for(i=0;i<m2;i++) b[i] = x[m1+i];
+	mergeSort(a,m1);
+	mergeSort(b,m2);
+}
+
 void main()
 {
 	int n,i;
@@ -13,6 +27,7 @@ void main()
 		printf("Enter %d numbered value: ",i+1);
 		scanf("%d",&x[i]);
 	}
+	mergeSort(x,n);
 	printf("Sorted Array is : \n");
 	display(x,n);
 }
