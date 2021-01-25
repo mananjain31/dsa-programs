54f8c1d150e2de55f985f83bca5c9a466af2d43f
mananjain31 <mananjain31jan@gmail.com>
2021-01-26 00:18:57 +0530

    insertionSort Function added

--- insertion-sort/insertionSort.c
+++ insertion-sort/insertionSort.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
+void insertionSort(int x[], int n)
+{
+	int i,j;
+	for(i=1; i<n; i++)
+	{
+		int value = x[i];
+		for(j=i-1;j>=0;j--)
+		{
+			if(value < x[j])
+				x[j+1] = x[j];
+			else break;
+		}
+		x[j+1] = value;
+	}
+}
 void display(int x[], int n)
 {
 	for(int i=0;i<n;i++) 
@@ -15,5 +30,6 @@ void main()
 		printf("Enter %d numbered value: ",i+1);
 		scanf("%d",&x[i]);
 	}
+	insertionSort(x, n);
 	display(x, n);
 }
