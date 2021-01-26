aae0d3cdb1ea03912084efe369f85c28ff286746
mananjain31 <mananjain31jan@gmail.com>
2021-01-27 00:44:22 +0530

    modified usingString.c

--- single-loop-pyramid/usingString.c
+++ single-loop-pyramid/usingString.c
@@ -1,2 +1,15 @@
 //Using String solving single-loop-pyramid 
 #include<stdio.h>
+#include<string.h>
+void main()
+{
+	int i;
+	int n = 5;
+	char s[n];
+	s[0]='*';
+	for(i=0;i<n;i++)
+	{	
+		printf("%s\n",s);	
+		strcat(s,"*");
+	}
+}
