8cd9596ff9e28b7974a2f67e98ce2e802ee55c8f
mananjain31 <mananjain31jan@gmail.com>
2021-01-27 23:49:32 +0530

    completed Max-heap.c

--- heap-sort/Max-heap.c
+++ heap-sort/Max-heap.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
+void display(int a[], int n);
+void show(int a[], int i, int n)
+{
+	int t,l = i*2 + 1, r = i*2 + 2;
+	if(r<n)
+		l = (a[l]>a[r])?l:r;
+	if(l<n && a[i]<a[l])
+	{
+		t=a[i];
+		a[i] = a[l];
+		a[l] = t;
+		display(a,n);
+		show(a,l,n);
+	}
+}
 void showLoop(int a[], int n)
 {
 	int i;
@@ -8,5 +23,15 @@ void showLoop(int a[], int n)
 void main()
 {
 	int i, a[]={1,2,3,4,5,6,7};
-	show(a,7);
+	int n = sizeof(a)/sizeof(a[0]);
+	showLoop(a,n);
+	display(a,n);
+}
+void display(int a[], int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
 }
