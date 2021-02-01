ee7e271e8721f7ed87a0b37538c874fda99cc8e7
Manan Jain <mananjain31jan@gmail.com>
2021-02-01 23:24:01 +0530

    modified countingSort.c

--- counting-sort/countingSort.c
+++ counting-sort/countingSort.c
@@ -2,26 +2,33 @@
 void countingSort(int x[], int n)
 {
 	int i,max = x[0];
-	for(i = 1; i<n; i++)
-		if(max<x[i]) 
-			max = x[i]; 
-	
+	for(i = 0; i<n; i++)
+	{
+		if(x[i] > max)
+			max = x[i];
+	}
+	max++;
 	int a[max];
-	for(i=0;i<max;i++)
+	for(i = 0; i<max; i++)
 		a[i] = 0;
 	
 	for(i = 0; i<n; i++)
 		a[x[i]]++;
 	
 	for(i = 1; i<max; i++)
-		a[i]+=a[i-1];
+		a[i] += a[i-1];
 	
-	for(i = 0; i<max; i++)
-		printf("%d ",a[i]);
-	printf("\n");
+	int y[n];
+	for(i = 0; i<n; i++)
+		y[ a[x[i]]-1 ] = x[i];
+	for(i=0;i<n;i++)
+		printf("%d ",y[i]);
+	
+	
 }
 void main()
 {
-	int x[] = {8,7,6,4,1,9};
-	countingSort(x,sizeof(x)/sizeof(x[0]));
+	int x[] = {8,7,6,4,1,9,12,-1};
+	int n = sizeof(x)/sizeof(x[0]);
+	countingSort(x,n);
 }
