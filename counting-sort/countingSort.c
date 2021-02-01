00ef8244a94240fdfe8cfa79f3114d4e6bc3fd83
Manan Jain <mananjain31jan@gmail.com>
2021-02-01 22:37:04 +0530

    _

---
+++ counting-sort/countingSort.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	int x[] = {8,7,6,4,1,9}
+}
---
+++ only-recursion-rectangle-problem/usingGobalVar.c
@@ -0,0 +1,12 @@
+/*
+	print rectange of r*c dimension
+	without using loop
+	using recursion only
+	and recursion of  function : show()
+	with 2 parameters is only allowed
+
+show(8,6)
+
+show(int r, int c)
+
+*/
