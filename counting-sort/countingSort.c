1328eb8768b4aa18aa7cd16edec136f35268a5c0
Manan Jain <mananjain31jan@gmail.com>
2021-02-02 23:50:50 +0530

    modified for -ve values

--- counting-sort/countingSort.c
+++ counting-sort/countingSort.c
@@ -1,37 +1,47 @@
 #include<stdio.h>
 void countingSort(int x[], int n)
 {
-	int i,max = x[0];
+	int i,min = x[0],max = x[0];
 	for(i = 0; i<n; i++)
 	{
 		if(x[i] > max)
 			max = x[i];
 	}
+	
+	for(i=1;i<n;i++)
+		if(min>x[i])
+			min = x[i];
+	
 	max++;
-	int a[max];
-	for(i = 0; i<max; i++)
+	
+	int a[max-min];
+	for(i=0; i<n; i++)
+		x[i] = x[i]-min;
+	
+	for(i = 0; i<max-min; i++)
 		a[i] = 0;
 	
 	for(i = 0; i<n; i++)
 		a[x[i]]++;
 	
-	for(i = 1; i<max; i++)
+	for(i = 1; i<max-min; i++)
 		a[i] += a[i-1];
 	
 	int y[n];
 	for(i = 0; i<n; i++)
 	{
-		y[a[x[i]]-1] = x[i];
-		a[x[i]]--;
+		y[a[x[i]]-=1] = x[i];
 	}
 	for(i=0;i<n;i++)
-		printf("%d ",y[i]);
-	
+		x[i] = y[i] + min;
 	
 }
 void main()
 {
-	int x[] = {8,7,6,4,1,9,1,20,2,3,3};
+	int i;
+	int x[] = {8,7,6,4,1,-9,1,20,2,-3,3};
 	int n = sizeof(x)/sizeof(x[0]);
 	countingSort(x,n);
+	for(i=0;i<n;i++)
+		printf("%d ",x[i]);
 }
