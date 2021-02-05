35771640db9dad13204171577790a2f4e7224edc
Manan Jain <mananjain31jan@gmail.com>
2021-02-05 22:12:00 +0530

    added display function

--- bubble-sort/bubbleSort.c
+++ bubble-sort/bubbleSort.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+void display(int a[], int n)
+{
+	int i;
+	for(i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
 void main()
 {
 	int a[]={6,5,3,1,9,7,2,4,1};
 	int n =sizeof(a)/sizeof(a[0]);
-	
+	display(a,n);
 }
