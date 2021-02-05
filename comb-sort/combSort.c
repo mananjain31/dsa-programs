19c27bf6ccba12dfd8f89764651149f9a8faec2b
Manan Jain <mananjain31jan@gmail.com>
2021-02-05 22:43:57 +0530

    added display function

--- comb-sort/combSort.c
+++ comb-sort/combSort.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+void display(int a[], int n)
+{
+	int i;
+	for(i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
 void main()
 {
-	int i,a={4,0,2,3,7,1,8,5,9,6}
+	int i,a[]={4,0,2,3,7,1,8,5,9,6};
 	int n = sizeof(a)/sizeof(a[0]);
+	display(a,n);
 }
