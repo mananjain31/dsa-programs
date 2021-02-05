f32ac69e655e29652c6d91110d86ff53225e9ca5
Manan Jain <mananjain31jan@gmail.com>
2021-02-05 22:52:10 +0530

    added combSort function

--- comb-sort/combSort.c
+++ comb-sort/combSort.c
@@ -6,9 +6,30 @@ void display(int a[], int n)
 		printf("%d ", a[i]);
 	printf("\n");
 }
+void combSort(int a[], int n)
+{
+	int gap,j;
+	for(gap = n/1.3; gap > 0; gap/=1.3)
+	{
+		for(j=gap; j<n; j++)
+		{
+			if(a[j] < a[j-gap])
+			{
+				a[j] = a[j] + a[j-gap];
+				a[j-gap] = a[j] - a[j-gap];
+				a[j] = a[j] - a[j-gap];
+			}
+			display(a,n);
+		}
+		printf("\n");
+	}
+}
 void main()
 {
 	int i,a[]={4,0,2,3,7,1,8,5,9,6};
 	int n = sizeof(a)/sizeof(a[0]);
+	printf("some output for understanding:\n");
+	combSort(a,n);
+	printf("\nsorted array:\n");
 	display(a,n);
 }
