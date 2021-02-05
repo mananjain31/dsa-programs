6d7cda207b62e00706768f618e4bba7971c3fb61
Manan Jain <mananjain31jan@gmail.com>
2021-02-05 22:23:51 +0530

    added bubbleSort function

--- bubble-sort/bubbleSort.c
+++ bubble-sort/bubbleSort.c
@@ -6,9 +6,30 @@ void display(int a[], int n)
 		printf("%d ", a[i]);
 	printf("\n");
 }
+void bubbleSort(int a[], int n)
+{
+	int i,j;
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n-1-i; j++)
+		{
+			if(a[j] > a[j+1])
+			{
+				int t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+			}
+			display(a,n);
+		}
+			printf("\n");
+	}
+}
 void main()
 {
 	int a[]={6,5,3,1,9,7,2,4,1};
 	int n =sizeof(a)/sizeof(a[0]);
+	printf("some output for understanding:\n");
+	bubbleSort(a,n);
+	printf("\nsorted array:\n");
 	display(a,n);
 }
