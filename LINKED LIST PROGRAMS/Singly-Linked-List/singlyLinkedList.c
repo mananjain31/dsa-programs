56f46832bd29060cbe1b27f902cc68276d8a3cc3
Manan Jain <mananjain31jan@gmail.com>
2021-02-10 15:12:58 +0530

    added insert at beggining function

--- LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
+++ LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
@@ -1,7 +1,35 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// struct Node
-// {
-	// int data;
-	// struct Node *next;
-// }
+#include<stdio.h>
+#include<stdlib.h>
+void insertAtB(int x);
+void showAll();
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+struct Node *start = NULL;
+void main()
+{
+	insertAtB(10);
+	insertAtB(20);
+	insertAtB(30);
+	insertAtB(40);
+	insertAtB(50);
+	showAll();
+}
+void insertAtB(int x)
+{
+	struct Node *temp = malloc(sizeof(struct Node));
+	temp->data = x;
+	temp->next = start;
+	start = temp;
+}
+void showAll()
+{
+	struct Node *temp = start;
+	while(temp!=NULL)
+	{
+		printf("%d\n",temp->data);
+		temp=temp->next;
+	}
+}
