172a9efb64dee8b1db72caf47ed049b455f032c6
Manan Jain <mananjain31jan@gmail.com>
2021-02-11 00:54:22 +0530

    completed selection sort in java

--- selection-sort/SelectionSort.java
+++ selection-sort/SelectionSort.java
@@ -3,7 +3,28 @@ class SelectionSort
 {
 	void display(int x[])
 	{
-		for(int i:x) System.out.println(i+" ");
+		for(int i:x) System.out.print(i+" ");
+	}
+	void swap(int x[], int i, int j)
+	{
+		x[i] = x[i]+x[j];
+		x[j] = x[i]-x[j];
+		x[i] = x[i]-x[j];
+	}
+	void sort(int x[])
+	{
+		int i,j,min;
+		int n=x.length;
+		for(i=0;i<n-1;i++)
+		{
+			min=i;
+			for(j=i+1;j<n;j++)
+				if(x[min]>x[j]) min = j;
+			if(min != i)
+			{
+				swap(x,i,min);
+			}				
+		}
 	}
 	public static void main(String...args)
 	{
