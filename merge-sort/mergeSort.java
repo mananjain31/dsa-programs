5c24aaa6bff79cf0618c366cb80757ea5e139ca9
Manan Jain <mananjain31jan@gmail.com>
2021-02-11 00:39:17 +0530

    completed merge sort in java

--- merge-sort/mergeSort.java
+++ merge-sort/mergeSort.java
@@ -5,12 +5,32 @@ class mergeSort
 	{
 		for(int i:x)
 		{
-			System.out.println(i);
+			System.out.print(i+" ");
 		}
 	}
-	void sort()
+	void merge(int a[],int b[],int x[])
 	{
-		
+		int i=0, j=0, k=0;
+		int m1=a.length, m2=b.length;
+		while(i<m1 && j<m2)
+			if(a[i]<b[j]) x[k++]=a[i++];
+			else x[k++]=b[j++];
+		while(i<m1) x[k++]=a[i++];
+		while(j<m2) x[k++]=b[j++];
+	}
+	void sort(int x[])
+	{
+		int n=x.length;
+		if(n==1)return;
+		int m1 = n/2;
+		int m2 = n-m1;
+		int a[] = new int[m1];
+		int b[] = new int[m2];
+		for(int i=0;i<m1;i++)a[i]=x[i];
+		for(int i=0;i<m2;i++)b[i]=x[i+m1];
+		sort(a);
+		sort(b);
+		merge(a,b,x);
 	}
 	public static void main(String...args)
 	{
@@ -26,9 +46,9 @@ class mergeSort
 		}
 		
 		mergeSort m =new mergeSort();
-		
+		m.sort(x);
+		System.out.println("Sorted using Merge Sort : ");
 		m.display(x);
-		// for(i=0;i<n;i++)
-			// System.out.println(x[i]);
+
 	}
 }
