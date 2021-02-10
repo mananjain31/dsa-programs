c817efea8dbf53f81a3fb6ac1b81b2475a20e0f5
Manan Jain <mananjain31jan@gmail.com>
2021-02-10 23:33:05 +0530

    added display function in mergeSort.java

---
+++ merge-sort/mergeSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class mergeSort
+{
+	void display(int x[])
+	{
+		for(int i:x)
+		{
+			System.out.println(i);
+		}
+	}
+	void sort()
+	{
+		
+	}
+	public static void main(String...args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n,i;
+		System.out.print("Enter No of values to be sorted :");
+		n=sc.nextInt();
+		int x[] = new int[n];
+		for(i=0;i<n;i++)
+		{
+			System.out.print("Enter "+(i+1)+" numbered value : ");
+			x[i]=sc.nextInt();
+		}
+		
+		mergeSort m =new mergeSort();
+		
+		m.display(x);
+		// for(i=0;i<n;i++)
+			// System.out.println(x[i]);
+	}
+}
