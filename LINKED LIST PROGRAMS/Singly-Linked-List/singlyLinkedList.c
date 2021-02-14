e099efc0a2345c0c123213608a5c7d71b4e09525
Manan Jain <mananjain31jan@gmail.com>
2021-02-14 20:46:28 +0530

    midified singly Linked List

--- LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
+++ LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
@@ -8,14 +8,35 @@ struct Node
 	struct Node *next;
 };
 struct Node *start = NULL;
-void main()
+int main()
 {
-	insertAtB(10);
-	insertAtB(20);
-	insertAtB(30);
-	insertAtB(40);
-	insertAtB(50);
-	showAll();
+	int run = 1;
+	int data, c=-1;
+	while(c)
+	{
+		//MAIN LINKED LIST CODE :
+		printf("\nSingly Linked List\n");
+		//Operations Available:
+		printf("0. Exit\n");
+		printf("1. Insert At Beginning\n");
+		printf("2. Show All Data\n");
+		printf("Enter Your Choice: ");
+		scanf("%d",&c);
+		switch(c)
+		{
+			case 1:
+				printf("Enter Data: ");
+				scanf("%d",&data);
+				insertAtB(data);
+				break;
+			case 2:
+				showAll();
+				break;
+			case 0:
+				break;
+		}
+	}
+	return 0;
 }
 void insertAtB(int x)
 {
@@ -29,7 +50,8 @@ void showAll()
 	struct Node *temp = start;
 	while(temp!=NULL)
 	{
-		printf("%d\n",temp->data);
+		printf("%d ",temp->data);
 		temp=temp->next;
 	}
+	printf("\n");
 }
