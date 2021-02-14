7a223431d299c9f2ac27850048e6553c7e6f2fbb
Manan Jain <mananjain31jan@gmail.com>
2021-02-14 22:41:18 +0530

    insert at specific position

--- LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
+++ LINKED LIST PROGRAMS/Singly-Linked-List/singlyLinkedList.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 void insertAtB(int x);
 void insertAtEnd(int x);
-// void insertAtSP(int x);
+void insertAtSPos(int x, int p);
+//void insertAfterSData(int x, int p);
 int countNodes();
 void showAll();
 struct Node
@@ -24,7 +25,7 @@ int main()
 		printf("2. Count Nodes\n");
 		printf("3. Insert At Beginning\n");
 		printf("4. Insert At Ending\n");
-		// printf("4. Insert Node at Specific Position \n");
+		printf("5. Insert Node at Specific Position \n");
 		printf("Enter Your Choice : ");
 		scanf("%d",&c);
 		switch(c)
@@ -47,14 +48,13 @@ int main()
 				scanf("%d",&data);
 				insertAtEnd(data);
 				break;
-			// case 5:
-				// printf("Enter Data : ");
-				// scanf("%d",&data);
-				// printf("Enter Position : ");
-				// scanf("%d",&pos);
-				// insertAtSP(data, pos);
-				// break;
-			
+			case 5:
+				printf("Enter Data : ");
+				scanf("%d",&data);
+				printf("Enter Position : ");
+				scanf("%d",&pos);
+				insertAtSPos(data, pos);
+				break;
 		}
 	}
 	return 0;
@@ -105,7 +105,30 @@ void insertAtEnd(int x)
 	temp -> next = temp2;
 }
 
-// void insertAtSP(int x,int p)
-// {
-	
-// }
+void insertAtSPos(int x,int p)
+{
+	if(p == 1)
+	{
+		insertAtB(x);
+	}
+	else if(p == countNodes() + 1)
+	{
+		insertAtEnd(x);		
+	}
+	else if(p > 1 && p <= countNodes())
+	{
+		struct Node *temp = start;
+		struct Node *temp2 = (struct Node*) malloc(sizeof(struct Node*));
+		for(int i = 1; i < p - 1; i++)
+		{
+			temp = temp -> next;
+		}
+		temp2 -> data = x;
+		temp2 -> next = temp -> next; 
+		temp -> next = temp2;
+	}
+	else
+	{
+		printf("Invalid Position\n");
+	}
+}
