da7edb654fe33f59d1bae7d3257d5748cb6edf64
Manan Jain <mananjain31jan@gmail.com>
2021-02-24 00:01:53 +0530

    completed infix to postfix program

--- STACK PROGRAMS/infixToPostfix.c
+++ STACK PROGRAMS/infixToPostfix.c
@@ -7,30 +7,43 @@ char pop();
 char peek();
 void push(char);
 bool empty();
+int priority(char);
 int top = -1;
 
-void main()
+void main ()
 {
 	int i, k = 0;
 	char input[CAPACITY];
 	char output[CAPACITY];
 	printf("Enter Expression : ");
 	scanf("%s",input);
-	for(i = 0; input[i] != '\0'; i++)
+	for (i = 0; input[i] != '\0'; i++)
 	{
-		if(input[i] >= 'A' && input[i] <= 'Z')
+		if (input[i] >= 'A' && input[i] <= 'Z')
 			output[k++] = input[i];
-		else 
+		else if (input[i] == ')' || input[i] == '(')
 		{
-			if(input[i] == '(') push(input[i]);
-			else if(input[i] == ')')
+			if (input[i] == '(') push(input[i]);
+			else if (input[i] == ')')
 			{
-				while(peek() != '(')
+				while (peek() != '(')
 					output[k++] = pop();
 				pop();
 			}
-			else push(input[i]);
+			else
+			{
+				
+			} 
 		}
+		else 
+		{
+			while (priority(input[i]) <= priority(peek()) && peek() != '(')
+			{
+				output[k++] = pop();
+			}
+			push(input[i]);
+		}
+
 	}
 	output[k] = '\0';
 	printf("%s", output);
@@ -54,4 +67,11 @@ char peek()
 bool empty()
 {
 	return (top == -1 ? true : false);
+}
+
+int priority(char x)
+{
+	if(x == '+' || x == '-')return 1;
+	if(x == '*' || x == '/')return 2;
+	if(x == '^')return 3;
 }
