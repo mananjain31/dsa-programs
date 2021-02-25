d7bcbb50626558bdf08e487ca87efb20fa5d2a11
Manan Jain <mananjain31jan@gmail.com>
2021-02-25 23:33:19 +0530

    Leniar Queue Program

---
+++ QUEUE PROGRAMS/LeniarQueue/usingArray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 5
+void enqueue(int x);
+void dequeue();
+void showAll();
+int front = -1
+int rear = -1
+int queue[MAX];
+int main()
+{
+	while(1)
+	{
+		int ch;
+		printf("\n......MENU......\n");
+		printf("1. Enqueue \n");
+		printf("2. Dequeue \n");
+		printf("3. Show All \n");
+		printf("4. Exit \n");
+		printf("Enter Your Choice : ");
+	}
+	return 1;
+}
