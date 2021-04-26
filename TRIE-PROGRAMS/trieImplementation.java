4f381f967f36d1a7fdafcfcd2c906cd0ebd612d3
Manan Jain <mananjain31jan@gmail.com>
2021-04-26 19:03:45 +0530

    added menu table in trieImplementation.java

--- TRIE-PROGRAMS/trieImplementation.java
+++ TRIE-PROGRAMS/trieImplementation.java
@@ -1,8 +1,43 @@
 import java.util.*;
 class trieImplementation
 {
-    public static void main(String[] args) {
-        Trie t = new Trie();  
+    public static void main(String[] args) 
+    {
+        Trie trie = new Trie();  
+        Scanner sc = new Scanner(System.in);
+        String s;
+        int choice = 0;
+        do
+        {
+            System.out.println(">>>>>>>> TRIE <<<<<<<<<");
+            System.out.println("0. Exit");
+            System.out.println("1. Insert String");
+            System.out.println("2. Delete String");
+            System.out.println("3. Count occurence");
+            System.out.print("Enter Your Choice : ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("Enter String To Insert : ");
+                    s = sc.nextLine();
+                    trie.insert(s);
+                    System.out.println("Inserted " + s + " Succesfully");
+                    break;
+                case 2:
+                    System.out.println("Enter String To Delete : ");
+                    s = sc.nextLine();
+                    trie.delete(s);
+                    System.out.println("Deleted " + s + " Succesfully");
+                    break;
+                case 3:
+                    System.out.println("Enter String To Count Its Occurence : ");
+                    s = sc.nextLine();
+                    System.out.println("occurence of " + s + " in the trie is  => " + trie.count(s));
+                    break;
+            }
+        }while(choice != 0);
     }
 }
 class Trie
@@ -47,8 +82,10 @@ class Trie
             char c = s.charAt(i);
             curr = curr.next(c);
             if(curr == null)
-                return;
+                throw new RuntimeException("String not found");
         }
+        if(curr.terminating == 0)
+            throw new RuntimeException("String not found");
         curr.terminating--;
     }
 }
