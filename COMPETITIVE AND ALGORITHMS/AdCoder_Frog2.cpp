d7d05eae06613b552e5f8998949b73f7e3793bca
Manan Jain <mananjain31jan@gmail.com>
2021-04-27 23:23:48 +0530

    AdCoder: B - Frog2

--- COMPETITIVE AND ALGORITHMS/AdCoder_Frog2.cpp
+++ COMPETITIVE AND ALGORITHMS/AdCoder_Frog2.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std ;
 #define ll long long int
-ll n;
+ll n, k;
 ll solve (ll *a, ll *dp, ll i)
 {
   if (i >= n) return LLONG_MAX;
   if (dp[i] != -1) return dp[i];
-  ll left = abs (a[i] - a[i + 1]) + solve (a, dp, i + 1);
-  ll right = LLONG_MAX;
-  if (i + 2 < n)
-    right = abs (a[i] - a[i + 2]) + solve (a, dp, i + 2);
-  return dp[i] = min (left, right);
+  
+  ll res = LLONG_MAX;
+  
+  for (int j = i + 1; j < min (n, i + k + 1); j++)
+    res = min (res , abs (a[i] - a[j]) + solve (a, dp, j));
+
+  return dp[i] = res;
 }
 int main ()
 {
@@ -18,6 +20,7 @@ int main ()
   cin.tie(0) ;
   cout.tie(0) ;
   cin >> n;
+  cin >> k;
   ll a[n];
   ll dp[n];
   for (int i=0; i<n; i++)
